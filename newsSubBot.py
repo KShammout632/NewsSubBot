f136e51bef0eb3ae7414d599782e66190cbeca05
Kareem Shammout <kareemshammout632@gmail.com>
2019-07-05 23:36:39 -0700

    Add files via upload
    Main file with json dictionary

---
+++ newsSubBot.py
@@ -0,0 +1,92 @@
+import tweepy, tkinter
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+import json
+
+consumer_key = 'V7wULbT8uiX2uWPiZOoxk2A8K'
+consumer_secret = 'tEt5viQLcc4ZLZaLzPAr4ZZt0TJ9SqxFp8qF0FpfXVFIrPgRRh'
+access_token = '2239643439-JuTlUGNiZyowncZGf9Pv7DrFPXqtJyjR1ziLEAK'
+access_token_secret = 'Nf3hse6f8QCl8zckNLo7vR5pNxiU7MVD3xSt58I9HNZL5'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+user = api.me()
+print(user.name)
+
+def retweet5():
+    search = "nature"
+    numOfTweets = 5
+    for tweet in tweepy.Cursor(api.search, search).items(numOfTweets):
+        try:
+            tweet.retweet()
+            print("Retweeted the tweet: " + tweet.text)
+        except tweepy.TweepError as e:
+            print(e.reason)
+        except StopIteration:
+            break
+
+def printFavs():
+    for s in api.favorites():
+        try:
+            print("Desc: " + s.text_extended)
+            print("With " + str(s.favorite_count) + " favourites")
+        except tweepy.TweepError as e:
+            print(e.reason)
+        except StopIteration:
+            break
+
+
+#url = 'http://web.mta.info/developers/turnstile.html'
+url = 'https://www.bbc.com/news/world'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+with open('newsSub.json', 'r') as json_file:
+    data = json.load(json_file)
+
+def getHeader():
+    desc = ""
+    link = ""
+    s1 = soup.find("a", class_="gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor")
+    allHeaders = soup.find_all("a", class_="gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor")
+    for header in allHeaders:
+        desc = header.string
+        #desc = "rebuild"
+        if(desc != None):
+            (key,val) = getMatchingWord(desc)
+        if(desc != None and val != ""):
+            print("Title of article: " + desc)
+            #print(getMatchingWord(desc))
+            tweetOut(header,key)
+            break
+
+def tweetOut(header,key):
+    desc = header.string
+    prevLink = header.find_parents("a")
+    print(prevLink)
+    #link = prevLink.href
+    ind = desc.index(key)
+    val = data.get(key)
+    valLen = len(val)
+    outT = desc[:ind] + val + desc[ind+valLen:]
+    print(outT)
+    #API.update_status(outT)
+    #print("Original article: " + str(link))
+
+def getMatchingWord(s):
+    for p in data:
+        if(p in s):
+            return (p,data.get(p))
+    #json_file = None
+    return ("","")
+
+def main():
+    (x,y) = getMatchingWord("rebuild")
+    print(x,y)
+    getHeader()
+
+main()
